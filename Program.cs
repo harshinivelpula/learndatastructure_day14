d13c0096d848c8fc19e1d998ec0ddc5fb49f1d06
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 19:49:27 +0530

    create stack

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace datastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Queue");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+            Console.ReadLine();
+        }
+    }
+}
