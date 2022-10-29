8a09778f6847117b586f1a224d9bd1aff93c110a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 20:24:44 +0530

    create queue

---
+++ Queue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructure
+{
+    internal class queue
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Queue");
+            enqueue Enqueue = new enqueue();
+            Enqueue.Enqueue(56);
+            Enqueue.Enqueue(30);
+            Enqueue.Enqueue(70);
+     
+            Enqueue.Display();
+            Console.ReadLine();
+        }
+    }
+}
