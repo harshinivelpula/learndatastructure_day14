bb430a793ec0f4a4783945de8f531afd71d40b72
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 20:09:35 +0530

    use peek pop

---
+++ peekpop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructure
+{
+    internal class peekpop
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Welcome to Stack And Queue");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+
+            stackpeek stackPeek = new stackpeek();
+            stackPeek.Peek();
+            stackPeek.Pop();
+            stackPeek.Display();
+            stackPeek.Peek();
+            stackPeek.Pop();
+            stackPeek.Display();
+            stackPeek.Peek();
+            stackPeek.Pop();
+            stackPeek.Display();
+
+            
+            Console.ReadLine();
+        }
+    }
+}
