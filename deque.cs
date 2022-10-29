3d64dd073fa375e5bd7ecb956c86d474c44c4673
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 20:46:36 +0530

    deque from begining

---
+++ deque.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructure
+{
+    internal class deque
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Welcome to Stack And Queue");
+            delete Delete = new delete();
+            Delete.Enqueue(56);
+            Delete.Enqueue(30);
+            Delete.Enqueue(70);
+            Delete.Display();
+
+            Delete.Dequeue();
+            Delete.Display();
+            Delete.Dequeue();
+            Delete.Display();
+            Delete.Dequeue();
+            Delete.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
